07edce04713a6677d4e07b35cb5e8f171ce4b507
Roxanne Kelly <89264198+Roxannekelly@users.noreply.github.com>
2023-02-15 20:36:21 -0500

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Homepage from './Homepage';
+import './index.css'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Homepage.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import About from './components/About'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import Portolio from './components/Portolio'
+
+const Homepage = () => {
+  return (
+    <div>
+        <Header />
+        <About />
+        <Portolio />
+        <Contact />
+        <Footer />
+
+    </div>
+  )
+}
+
+export default Homepage
