f057a7273dded3ea1fffb413202a12b90655a9af
Roxanne Kelly <Roxannekelly@rocketmail.com>
2023-12-12 13:29:06 -0500

    update dev

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,39 +1,36 @@
 import React from "react";
 import About from "../About";
+import NavBar from "./Nav";
 const logo = require("../../assets/Kelly.jpg");
-
 
 const Header = () => {
   return (
-    <div className="lg:flex">
-      <div className="lg:pr-32">
-        <img
-          src={logo}
-          className=" my-4 block mx-auto w-56 h-60 rounded-full lg:mx-16 lg:w-96 lg:h-96"
-        />
-      </div>
-      <div className="">
-        <div className="py-4">
-          <h1 className="uppercase text-center font-extrabold text-4xl">
-            I'm Roxanne Kelly
-          </h1>
-          <h1 className="uppercase text-center font-extrabold text-3xl">
-            Frontend Developer
-          </h1>
-        </div>
-        <div className="lg:mx-36">
-          <p className="text-center pb-6">
-            I'm a Jamaican based React Developer & front‑end developer focused
-            on crafting clean & user‑friendly experiences, I am passionate about
-            building excellent software that improves the lives of those around
-            me.
-          </p>
-        </div>
-        <div className="flex justify-center py-4">
-          <button className="inline-block  px-8 py-4 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">
-            {" "}
-            More About me{" "}
-          </button>
+    <div className="mt-28 lg:flex">
+      <NavBar />
+      <div className="flex h-screen w-full text-gray-50">
+        <div className="flex-1 bg-hero bg-cover bg-center bg-no-repeat">
+          <div className="px-2 border-4 space-y-9">
+            <div className="py-6 border-4 ">
+              <h1 className="uppercase font-bold text-7xl">
+                Designer/<span className="block">Developer</span>
+              </h1>
+            </div>
+            <div className="border-4 lg:mx-36">
+              <p className="pb-6 text-left text-3xl font-medium leading-normal">
+                {" "}I AM A REACT DEVELOPER AND GRAPHIC DESIGNER BASED IN JAMAICA. I
+                HAVE ONE YEAR OF EXPERIENCE IN CRAFTING CLEAN & USER-FRIENDLY
+                ALL AREAS OF DIGITAL. I LOVE MINIMAL AND BRUTALIST DESIGN. I
+                LOVE NATURE, MUSIC AND I AM PASSIONATE ABOUT BUILDING CREATIVE
+                DESIGNS.
+              </p>
+            </div>
+            <div className="flex justify-end py-4 border-4 px-2">
+              <button className="inline-block h-16 px-8 py-4 bg-slate-700 text-white font-medium text-lg leading-tight uppercase rounded-full shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">
+                {" "}
+                More About me{" "}
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
