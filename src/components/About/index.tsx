39c27a2e7738a976aae05932ecb685326a7266db
Roxanne Kelly <Roxannekelly@rocketmail.com>
2023-12-12 20:47:51 -0500

    update About Section

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -1,142 +1,234 @@
-import React from 'react'
-const Resume = require ('../../assets/Resume.pdf')
+import React, { useEffect, useState, useRef, ChangeEvent } from "react";
+const Resume = require("../../assets/Resume.pdf");
 
-const About =() =>{
+const About = () => {
+  const [scrollTop, setScrollTop] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollTop(window.scrollY);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
-    <div className='my-4'>
-        <div className='py-4'>
-            <h1 className='uppercase text-center text-5xl font-extrabold pb-16 '>About Me</h1>
+    <div className="border-4 bg-black text-white  border-red-700">
+      <div
+        className={`my-4 border-4 mx-6 transition-opacity opacity-0 duration-1000 delay-300 ease-in-out ${
+          scrollTop
+            ? "opacity-100 border-4 border-green-700"
+            : " opacity-0 ease-out"
+        }`}
+      >
+        <div className="py-4">
+          <h1 className="uppercase text-7xl font-extrabold pb-16 ">
+            Hello <span className="block">I'm Roxanne</span>
+          </h1>
         </div>
-            <div className=' flex flex-row justify-around'>
-               <div className=''> 
-                    <h1 className='uppercase text-2xl font-bold pl-4'>Personal Infos
-                    </h1>
-                    <div className='flex justify-between p-4'>
-                        <div className='flex flex-col'>
-                            <span className='py-1 text-xl font-semibold'>First Name: Roxanne</span>                  
-                            <span className='py-1 text-xl font-semibold'>Age: 27 Years</span>              
-                            <span className='py-1 text-xl font-semibold'>Phone: +1874318751</span>
-                        </div>
-                    </div>
-                </div> 
-
-                <div className=''>
-                    <div className='flex flex-col pt-10'>
-                        <span className='py-1 text-xl font-semibold'>Last Name: Steve</span>                  
-                        <span className='py-1 text-xl font-semibold'>Nationality: Jamaican</span> 
-                        <span className='py-1 text-xl font-semibold'>Email: Roxannekel71@gmail.com</span>            
-                        <span className='py-1 text-xl font-semibold'>Address: 514 Silverstone Greater Portmore</span>
-                    </div>  
-                    </div>       
+        <div className=" flex flex-row text-4xl justify-around">
+          <p>
+            I USE MY PASSION AND SKILLS TO CREATE AMAZING DIGITAL PRODUCTS AND
+            EXPERIENCES. i HAVE WORKED WITH COMPANYNATIONAL AND INTERNATIONAL CUSTOMERS RELY ON ME FOR
+            DESIGN, IMPLEMENTATION, AND MANAGEMENT OF THEIR DIGITAL PRODUCTS.
+          </p>
         </div>
-        <div className='flex justify-center py-4'>
-            <a href={Resume}>
-           <button className='block px-8 py-4 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out'> Download CV </button>
-            </a>
-            </div>    
-        <div className='py-4 '>
-            <h1 className='uppercase text-xl font-bold text-center'>My Skills</h1>
-            <div className='pl-4'>
-                <div className="w-full bg-gray-200 rounded-full my-5">
-                    <div className="bg-blue-800 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-l-full" style={{width: '90%'}}> 90%</div>
-                </div>
-                <span className='uppercase text-lg font-semibold'> HTML</span> 
-            </div>
-            <div className='pl-4'>
-                <div className="w-full bg-gray-200 rounded-full my-5">
-                    <div className="bg-blue-800 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-l-full" style={{width: '75%'}}> 75%</div>
-                </div>
-                <span className='uppercase text-lg font-semibold'> Javascript</span> 
-            </div>
-            <div className='pl-4'>
-                <div className="w-full bg-gray-200 rounded-full my-5">
-                    <div className="bg-blue-800 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-l-full" style={{width: '90%'}}> 90%</div>
-                </div>
-                <span className='uppercase text-lg font-semibold'> css</span> 
-            </div>
-            <div className='pl-4'>
-                <div className="w-full bg-gray-200 rounded-full my-5">
-                    <div className="bg-blue-800 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-l-full" style={{width: '60%'}}> 60%</div>
-                </div>
-                <span className='uppercase text-lg font-semibold'> React</span> 
-            </div>
-            <div className='pl-4'>
-                <div className="w-full bg-gray-200 rounded-full my-5">
-                    <div className="bg-blue-800 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-l-full" style={{width: '50%'}}> 50%</div>
-                </div>
-                <span className='uppercase text-lg font-semibold'> Tailwind</span> 
-            </div>
-            <div className='pl-4'>
-                <div className="w-full bg-gray-200 rounded-full my-5">
-                    <div className="bg-blue-800 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-l-full" style={{width: '50%'}}> 50%</div>
-                </div>
-                <span className='uppercase text-lg font-semibold'> Python</span> 
-            </div>
+        <div className="flex justify-center py-4 my-10">
+          <a href={Resume}>
+            <button className="block px-8 h-16 py-4 bg-blue-600 text-white font-medium text-lg leading-tight uppercase rounded-full shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">
+              {" "}
+              Download CV{" "}
+            </button>
+          </a>
         </div>
-    <div className='lg:flex'>
-        <div className=''>
-            <h1 className='uppercase text-2xl font-bold text-center'>Education</h1>
-            <div className='ml-4'>
-               
-                <div className='mb-8'>
-                    <h1 className="bg-black rounded-full w-44 text-center font-bold text-white "> Sept 2008 - July 2013</h1>
-                    <h1 className='uppercase py-2 font-bold text-lg'>Norman Manley High School</h1>
-                    <p className='font-semibold'>
-                    Lorem ipsum dolor sit amet, consectetur tempor incididunt ut labore adipisicing elit
-                    </p>
-                </div>
-                <div className='mb-8'>
-                    <h1 className="bg-black rounded-full w-44 text-center font-bold text-white "> Sept 2013 - July 2015</h1>
-                    <h1 className='uppercase py-2 font-bold text-lg'>St. Andrew Technical High School</h1>
-                    <p className='font-semibold'>
-                    Lorem ipsum dolor sit amet, consectetur tempor incididunt ut labore adipisicing elit
-                    </p>
-                </div>
-                <div className='mb-8'>
-                    <h1 className="bg-black rounded-full w-44 text-center font-bold text-white "> July 2017 - Dec 2017</h1>
-                    <h1 className='uppercase py-2 font-bold text-lg'>Vector Technology Institute</h1>
-                    <p className='font-semibold'>
-                    Lorem ipsum dolor sit amet, consectetur tempor incididunt ut labore adipisicing elit
-                    </p>
-                </div>
+        <div className="py-4 ">
+          <h1 className="uppercase text-xl font-bold text-center">Languages</h1>
+          <div className="pl-4">
+            <div className="w-full bg-gray-200 rounded-full my-5">
+              <div
+                className="bg-blue-800 h-8 text-lg font-medium text-blue-100 text-center p-0.5 leading-none rounded-l-full"
+                style={{ width: "90%" }}
+              >
+                {" "}
+                90%
+              </div>
             </div>
-        </div>
-        <div className=''>
-            <h1 className='uppercase text-2xl font-bold text-center'>Experience</h1>
-            <div className='ml-4'>
-                <div className=' mb-8'>
-                    <h1 className="bg-black rounded-full w-44 text-center font-bold text-white "> Jan 2017 - Sept 2022</h1>
-                    <h1 className='uppercase py-2 font-bold text-lg'>Graphic Designer</h1>
-                    <p className='font-semibold'>
-                    Lorem ipsum dolor sit amet, consectetur tempor incididunt ut labore adipisicing elit
-                    </p>
-                </div>
-                <div className=' mb-8'>
-                    <h1 className="bg-black rounded-full w-44 text-center font-bold text-white "> Mar 2016 - present</h1>
-                    <h1 className='uppercase py-2 font-bold text-lg'>Record Officer</h1>
-                    <p className='font-semibold'>
-                    Lorem ipsum dolor sit amet, consectetur tempor incididunt ut labore adipisicing elit
-                    </p>
-                </div>
-                <div className=' mb-8'>
-                    <h1 className="bg-black rounded-full w-44 text-center font-bold text-white "> Jul 2021 - Aug 2021</h1>
-                    <h1 className='uppercase py-2 font-bold text-lg'>Graphic Design Intern</h1>
-                    <p className='font-semibold'>
-                    Lorem ipsum dolor sit amet, consectetur tempor incididunt ut labore adipisicing elit
-                    </p>
-                </div>
-                <div className=' mb-8'>
-                    <h1 className="bg-black rounded-full w-44 text-center font-bold text-white "> Mar 2022 - Sept 2022</h1>
-                    <h1 className='uppercase py-2 font-bold text-lg'>Security Intern</h1>
-                    <p className='font-semibold'>
-                    Lorem ipsum dolor sit amet, consectetur tempor incididunt ut labore adipisicing elit
-                    </p>
-                </div>
+            <span className="uppercase text-lg font-semibold"> HTML</span>
+          </div>
+          <div className="pl-4">
+            <div className="w-full bg-gray-200 rounded-full my-5">
+              <div
+                className="bg-blue-800 h-8 text-lg font-medium text-blue-100 text-center p-0.5 leading-none rounded-l-full"
+                style={{ width: "75%" }}
+              >
+                {" "}
+                75%
+              </div>
             </div>
+            <span className="uppercase text-lg font-semibold"> Javascript</span>
+          </div>
+          <div className="pl-4">
+            <div className="w-full bg-gray-200 rounded-full my-5">
+              <div
+                className="bg-blue-800 h-8 text-lg font-medium text-blue-100 text-center p-0.5 leading-none rounded-l-full"
+                style={{ width: "90%" }}
+              >
+                {" "}
+                90%
+              </div>
+            </div>
+            <span className="uppercase text-lg font-semibold"> css</span>
+          </div>
+          <div className="pl-4">
+            <div className="w-full bg-gray-200 rounded-full my-5">
+              <div
+                className="bg-blue-800 h-8 text-lg font-medium text-blue-100 text-center p-0.5 leading-none rounded-l-full"
+                style={{ width: "60%" }}
+              >
+                {" "}
+                60%
+              </div>
+            </div>
+            <span className="uppercase text-lg font-semibold"> React</span>
+          </div>
+          <div className="pl-4">
+            <div className="w-full bg-gray-200 rounded-full my-5">
+              <div
+                className="bg-blue-800 h-8 text-lg font-medium text-blue-100 text-center p-0.5 leading-none rounded-l-full"
+                style={{ width: "50%" }}
+              >
+                {" "}
+                50%
+              </div>
+            </div>
+            <span className="uppercase text-lg font-semibold"> Tailwind Css</span>
+          </div>
+          <div className="pl-4">
+            <div className="w-full bg-gray-200 rounded-full my-5">
+              <div
+                className="bg-blue-800 h-8 text-lg font-medium text-blue-100 text-center p-0.5 leading-none rounded-l-full"
+                style={{ width: "50%" }}
+              >
+                {" "}
+                50%
+              </div>
+            </div>
+            <span className="uppercase text-lg font-semibold"> Python</span>
+          </div>
         </div>
-     </div>   
+        <div className="lg:flex">
+          {/* <div className="">
+            <h1 className="uppercase text-2xl font-bold text-center">
+              Education
+            </h1>
+            <div className="ml-4">
+              <div className="mb-8">
+                <h1 className="bg-black rounded-full w-44 text-center font-bold text-white ">
+                  {" "}
+                  Sept 2008 - July 2013
+                </h1>
+                <h1 className="uppercase py-2 font-bold text-lg">
+                  Norman Manley High School
+                </h1>
+                <p className="font-semibold">
+                  Lorem ipsum dolor sit amet, consectetur tempor incididunt ut
+                  labore adipisicing elit
+                </p>
+              </div>
+              <div className="mb-8">
+                <h1 className="bg-black rounded-full w-44 text-center font-bold text-white ">
+                  {" "}
+                  Sept 2013 - July 2015
+                </h1>
+                <h1 className="uppercase py-2 font-bold text-lg">
+                  St. Andrew Technical High School
+                </h1>
+                <p className="font-semibold">
+                  Lorem ipsum dolor sit amet, consectetur tempor incididunt ut
+                  labore adipisicing elit
+                </p>
+              </div>
+              <div className="mb-8">
+                <h1 className="bg-black rounded-full w-44 text-center font-bold text-white ">
+                  {" "}
+                  July 2017 - Dec 2017
+                </h1>
+                <h1 className="uppercase py-2 font-bold text-lg">
+                  Vector Technology Institute
+                </h1>
+                <p className="font-semibold">
+                  Lorem ipsum dolor sit amet, consectetur tempor incididunt ut
+                  labore adipisicing elit
+                </p>
+              </div>
+            </div>
+          </div> */}
+          {/* <div className="">
+            <h1 className="uppercase text-2xl font-bold text-center">
+              Experience
+            </h1>
+            <div className="ml-4">
+              <div className=" mb-8">
+                <h1 className="bg-black rounded-full w-44 text-center font-bold text-white ">
+                  {" "}
+                  Jan 2017 - Sept 2022
+                </h1>
+                <h1 className="uppercase py-2 font-bold text-lg">
+                  Graphic Designer
+                </h1>
+                <p className="font-semibold">
+                  Lorem ipsum dolor sit amet, consectetur tempor incididunt ut
+                  labore adipisicing elit
+                </p>
+              </div>
+              <div className=" mb-8">
+                <h1 className="bg-black rounded-full w-44 text-center font-bold text-white ">
+                  {" "}
+                  Mar 2016 - present
+                </h1>
+                <h1 className="uppercase py-2 font-bold text-lg">
+                  Record Officer
+                </h1>
+                <p className="font-semibold">
+                  Lorem ipsum dolor sit amet, consectetur tempor incididunt ut
+                  labore adipisicing elit
+                </p>
+              </div>
+              <div className=" mb-8">
+                <h1 className="bg-black rounded-full w-44 text-center font-bold text-white ">
+                  {" "}
+                  Jul 2021 - Aug 2021
+                </h1>
+                <h1 className="uppercase py-2 font-bold text-lg">
+                  Graphic Design Intern
+                </h1>
+                <p className="font-semibold">
+                  Lorem ipsum dolor sit amet, consectetur tempor incididunt ut
+                  labore adipisicing elit
+                </p>
+              </div>
+              <div className=" mb-8">
+                <h1 className="bg-black rounded-full w-44 text-center font-bold text-white ">
+                  {" "}
+                  Mar 2022 - Sept 2022
+                </h1>
+                <h1 className="uppercase py-2 font-bold text-lg">
+                  Security Intern
+                </h1>
+                <p className="font-semibold">
+                  Lorem ipsum dolor sit amet, consectetur tempor incididunt ut
+                  labore adipisicing elit
+                </p>
+              </div>
+            </div>
+          </div> */}
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,15 +7,15 @@ const Header = () => {
   return (
     <div className="mt-28 lg:flex">
       <NavBar />
-      <div className="flex h-screen w-full text-gray-50">
+      <div className="flex h-screen w-full text-gray-50 scroll-smooth">
         <div className="flex-1 bg-hero bg-cover bg-center bg-no-repeat">
-          <div className="px-2 border-4 space-y-9">
-            <div className="py-6 border-4 ">
+          <div className="px-2 space-y-9">
+            <div className="py-6">
               <h1 className="uppercase font-bold text-7xl">
                 Designer/<span className="block">Developer</span>
               </h1>
             </div>
-            <div className="border-4 lg:mx-36">
+            <div className="lg:mx-36">
               <p className="pb-6 text-left text-3xl font-medium leading-normal">
                 {" "}I AM A REACT DEVELOPER AND GRAPHIC DESIGNER BASED IN JAMAICA. I
                 HAVE ONE YEAR OF EXPERIENCE IN CRAFTING CLEAN & USER-FRIENDLY
@@ -24,7 +24,7 @@ const Header = () => {
                 DESIGNS.
               </p>
             </div>
-            <div className="flex justify-end py-4 border-4 px-2">
+            <div className="flex justify-end py-4 px-2">
               <button className="inline-block h-16 px-8 py-4 bg-slate-700 text-white font-medium text-lg leading-tight uppercase rounded-full shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">
                 {" "}
                 More About me{" "}
