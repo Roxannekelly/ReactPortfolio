4ba0a60c522363a852c2875361cb2b1854214f31
Roxanne Kelly <Roxannekelly@rocketmail.com>
2023-12-14 20:51:16 -0500

    update nav

--- src/components/Header/Nav/index.tsx
+++ src/components/Header/Nav/index.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const NavBar = () => {
   return (
-    <div className='flex border-b-2 justify-between px-4 bg-hero text-white opacity-90 z-40 backdrop-blur fixed top-0 w-full shadow-md py-4'>
+    <div className='flex border-b-2 justify-around px-4 bg-hero text-white opacity-90 z-40 backdrop-blur fixed top-0 w-full shadow-md py-4'>
         <div className=''>
             <h2 className='text-lg font-bold'>Roxanne Kelly</h2>
-            <h1 className='uppercase font text-xl'>Graphic Designer/ Developer</h1>
+            <h1 className='uppercase font text-lg'>Graphic Designer/Developer</h1>
         </div>
         <div className='space-x-3 text-lg font-semibold'> 
             <span> portfolio</span>
